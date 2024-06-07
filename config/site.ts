90b67e51b50c6dae0ce94d2c4c908363cfbe5c94
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-07 09:21:44 +0530

    modified site title

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "ProJO",
+  description:
+    "Collabarate, manage and track your projects with ease. From high level overview to task level details, we have you covered.",
+};
