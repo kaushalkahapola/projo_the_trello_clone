03a3650748f7a63f15bb4867dd6ec70c38ef3b8c
Kaushal Kahapola <kaushal.21@cse.mrt.ac.lk>
2024-06-07 10:00:08 +0530

    add authentication using clerk

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,13 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+const platformLayout = (
+    {children}: {children: React.ReactNode}
+) => {
+  return (
+    <ClerkProvider>
+        {children}
+    </ClerkProvider>
+  )
+}
+
+export default platformLayout
